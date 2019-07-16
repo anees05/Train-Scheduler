e3785af2212e10620d6e339591859b4c8775b53e
anees05 <anees.adams05@gmail.com>
2019-07-15 20:53:59 -0400

    Creating object with key values and storing those values

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var trainData = firebase.database();
+
+  $("#addTrainBtn").on("click", function(){
+      var trainName = $("#trainNameInput").val().trim()
+      var destination = $("#destinationInput").val().trim()
+      var firstTrain = $("#firstTrainInput").val().trim()
+      var frequency = $("#frequencyInput").val().trim()
+
+      var newTrain = {
+          name: trainName,
+          destinationa: destination,
+          firstTrain: firstTrain,
+          frequency: frequency
+      }
+
+      alert("Train was Added")
+
+      console.log(trainName)
+      console.log(destination)
+      console.log(firstTrain)
+      console.log(frequency)
+  });
