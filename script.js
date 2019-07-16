dde91c8f2fb9a83de2b7084a25402c0e21f1ff7b
anees05 <anees.adams05@gmail.com>
2019-07-15 20:21:48 -0400

    Adding firebase

---
+++ script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA6Nct5KirJY_353jJgj5jMeOftxrVNVfw",
+    authDomain: "fb-demo-8143d.firebaseapp.com",
+    databaseURL: "https://fb-demo-8143d.firebaseio.com",
+    projectId: "fb-demo-8143d",
+    storageBucket: "",
+    messagingSenderId: "263940671261",
+    appId: "1:263940671261:web:0b6e45eb91b34a9a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
